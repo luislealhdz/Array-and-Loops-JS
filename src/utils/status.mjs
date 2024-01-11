69374ab871c0f5ff2a4ed7e695819a2c471998aa
0HungryDeveloper <hungrystudentdev@gmail.com>
2024-01-11 14:12:54 -0600

    🎯 FEAT / Use slice to access the two first users of the list

--- src/utils/status.mjs
+++ src/utils/status.mjs
@@ -7,21 +7,23 @@ export const onlineStatus = (friendsList) => {
     // No friends are online
     if (friendsList.length === 0) {
         return `No one is online`;
-    }
+    } else {
+        const firstUsers = friendsList.slice(0, 2).join(", ");
 
-    // Only one friend is online
-    if (friendsList.length === 1) {
-        return `${friendsList[0]} is online`;
-    }
+        // Only one friend is online
+        if (friendsList.length === 1) {
+            return `${friendsList[0]} is online`;
+        }
 
-    // Only two friends are online
-    if (friendsList.length === 2) {
-        return `${friendsList[0]} and ${friendsList[1]} are online`;
-    }
+        // Only two friends are online
+        if (friendsList.length === 2) {
+            return `${firstUsers} are online`;
+        }
 
-    // There are more than two friends online
-    if (friendsList.length >= 2) {
-        const restOfFriends = friendsList.length - 2;
-        return `${friendsList[0]} and ${friendsList[1]} and ${restOfFriends} more online`;
+        // There are more than two friends online
+        if (friendsList.length >= 2) {
+            const restOfFriends = friendsList.length - 2;
+            return `${firstUsers} and ${restOfFriends} more online`;
+        }
     }
 };
